41e276aa38e865d8568d067bf54862fdc6befaf1
Guido Tournois <guido.tournois@vantage-ai.com>
2020-10-16 17:58:01 +0200

    Clean up code of all modules

--- 1-python-wrapper/model/model.py
+++ 1-python-wrapper/model/model.py
@@ -5,10 +5,10 @@ import joblib
 import pandas as pd
 from training.train import train
 from sklearn.pipeline import Pipeline
-from typing import Tuple, Any
-from pathlib import Path
+from typing import Tuple, Dict
 
 logger = logging.getLogger(__name__)
+
 
 def load_json(filepath):
     with open(filepath) as json_file:
@@ -16,7 +16,9 @@ def load_json(filepath):
     return data
 
 
-def load_most_recent_model(model_dir: str) -> Tuple[Any, Any, Any]:
+def load_most_recent_model(
+    model_dir: str,
+) -> Tuple[Pipeline, Dict[str, str], Dict[str, str]]:
     """load model from file. If it does not yet exist, train a model
 
     Args:
@@ -32,12 +34,14 @@ def load_most_recent_model(model_dir: str) -> Tuple[Any, Any, Any]:
         model_dir = os.path.join(model_dir, version)
 
         return (
-            joblib.load(os.path.join(model_dir, 'model.joblib')),
-            load_json(os.path.join(model_dir, 'schema.json')),
-            load_json(os.path.join(model_dir, 'metadata.json')))
+            joblib.load(os.path.join(model_dir, "model.joblib")),
+            load_json(os.path.join(model_dir, "schema.json")),
+            load_json(os.path.join(model_dir, "metadata.json")),
+        )
 
     except Exception as e:
-        logger.info("Create model")
+        logger.error(e)
+        logger.info("No model found, let's train one!")
         train(model_dir=model_dir)
         return load_most_recent_model(model_dir)
 
@@ -51,7 +55,8 @@ def predict(data: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Output dataframe
     """
-    return pd.DataFrame(data={'prediction': model.predict(data)})
+    return pd.DataFrame(data={"prediction": model.predict(data)})
 
-model_path = os.path.join(os.path.dirname(__file__), '../models')
-model = load_most_recent_model(model_path)
+
+model_path = os.path.join(os.path.dirname(__file__), "../models")
+model, schema, metadata = load_most_recent_model(model_path)
--- 1-python-wrapper/wrapper/main.py
+++ 1-python-wrapper/wrapper/main.py
@@ -8,30 +8,47 @@ from model.model import load_most_recent_model
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
-@app.route('/predict', methods=["GET", "POST"])
+
+@app.route("/predict", methods=["POST"])
 def predict():
-    if request.method == "POST":
-        try:
-            input = request.json
-            data = pd.DataFrame(input, index=[0])
-            return {"classification": str(model.predict(data)[0])}
-        except Exception as err:
-            return {"error": err}
-    else:
-        return f"Please provide post request according to the schema <br> {schema}"
+    """
+    Predict endpoint.
+
+    Returns:
+        Output of request. A json containing either the field `classification`
+        or `error` in case of an error.
+    """
+    try:
+        input = request.json
+        data = pd.DataFrame(input, index=[0])
+        return {"classification": str(model.predict(data)[0])}
+    except Exception as err:
+        return {"error": err}
 
 
-@app.route('/schema', methods=["GET"])
+@app.route("/schema", methods=["GET"])
 def schema():
-    return schema
+    """
+    Schema endpoint
+
+    Returns:
+        The schema for the predict method
+    """
+    return _schema
 
 
-@app.route('/metadata', methods=["GET"])
+@app.route("/metadata", methods=["GET"])
 def metadata():
-    return metadata
+    """
+    Metadata endpoint
+
+    Returns:
+        Metadata of the model that is used for predicting.
+    """
+    return _metadata
 
 
-if __name__=="__main__":
-    model_path = os.path.join(os.path.dirname(__file__), '../models')
-    model, schema, metadata = load_most_recent_model(model_path)
+if __name__ == "__main__":
+    model_path = os.path.join(os.path.dirname(__file__), "../models")
+    model, _schema, _metadata = load_most_recent_model(model_path)
     app.run(debug=True)
